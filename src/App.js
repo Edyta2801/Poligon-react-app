bc574b761703b1cee9ec65075d7a0d44b81ed9a3
Edyta2801 <edyta.szarowska@interia.pl>
2020-10-24 18:42:31 +0200

    no wrap -2 solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import './App.css';
 
 
@@ -13,11 +13,11 @@ function News({ header, intro }) {
 
 function App() {
   return (
-    <Fragment>
+    <>
       <News header="Nagłówek 1" intro="Intro 1" />
       <News header="Nagłówek 2:Breaking news" intro="Intro 2" />
       <News header="Nagłówek 3: Nie śpię, bo trzymam kredens" intro="Intro 3" />
-    </Fragment>
+    </>
   );
 }
 
