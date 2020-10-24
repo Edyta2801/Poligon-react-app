2d6b675debb4bb74dfc8f6092d22c55bd575b3fa
Edyta2801 <edyta.szarowska@interia.pl>
2020-10-24 21:07:20 +0200

    iteration in jsx (map)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 
-
 function News({ header, intro }) {
   return (
     <div>
@@ -11,13 +10,26 @@ function News({ header, intro }) {
   );
 }
 
+const data = [
+  {
+    header: 'Nagłówek 1', intro: 'Intro 1',
+  }, {
+    header: 'Nagłówek 2: Breaking news', intro: "Intro 2",
+  }, {
+    header: 'Nagłówek 3: Nie śpię bo trzymam kresdens', intro: "Intro 3",
+  }
+];
+
 function App() {
   return (
-    <>
-      <News header="Nagłówek 1" intro="Intro 1" />
+    <div>
+      {data.map((elem, index) => (
+        <News key={`news-${index}`} header={elem.header} intro={elem.intro} />
+      ))}
+      {/* <News header="Nagłówek 1" intro="Intro 1" />
       <News header="Nagłówek 2:Breaking news" intro="Intro 2" />
-      <News header="Nagłówek 3: Nie śpię, bo trzymam kredens" intro="Intro 3" />
-    </>
+      <News header="Nagłówek 3: Nie śpię, bo trzymam kredens" intro="Intro 3" />*/}
+    </div>
   );
 }
 
