00a2d430d38a46e4db8112c238ac799dedd8181a
Edyta2801 <edyta.szarowska@interia.pl>
2020-10-24 18:41:07 +0200

    no wrap <div> in App - 1 solution

--- src/App.js
+++ src/App.js
@@ -1,40 +1,25 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import './App.css';
 
-const DAY = 'monday';
 
 function News({ header, intro }) {
   return (
     <div>
       <h2>{header}</h2>
       <p>{intro}</p>
-
     </div>
   );
 }
 
 function App() {
-  const checkDay = () => {
-    if (DAY == 'monday') {
-
-      return (
-        <div>
-          {DAY}
-          <News header="Nagłówek 1" intro="Intro 1" />
-          <News header="Nagłówek 2:Breaking news" intro="Intro 2" />
-          <News header="Nagłówek 3: Nie śpię, bo trzymam kredens" intro="Intro 3" />
-        </div>
-      );
-    }
-    return (
-      <p>Nie ma newsów z poniedziałku</p>
-    );
-  };
   return (
-    <div>
-      {checkDay()}
-    </div>
+    <Fragment>
+      <News header="Nagłówek 1" intro="Intro 1" />
+      <News header="Nagłówek 2:Breaking news" intro="Intro 2" />
+      <News header="Nagłówek 3: Nie śpię, bo trzymam kredens" intro="Intro 3" />
+    </Fragment>
   );
 }
+
 
 export default App;
