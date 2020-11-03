9ea9a8b20215c680e4862d327e5a18de1b80ff0c
Edyta2801 <edyta.szarowska@interia.pl>
2020-11-03 23:21:32 +0100

    add Header components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './App.css';
 import News from './pages/Articles.js';
+import Header from './components/Header';
 
 
 const data = [
@@ -17,6 +18,7 @@ const data = [
 function App() {
   return (
     <div>
+      <Header/>
       {data.map((elem) => (
         <News
           key={`news-${elem.id}`}
