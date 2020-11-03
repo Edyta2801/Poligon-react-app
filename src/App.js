4918728eb22fb1f3575b84546022efa1a104a976
Edyta2801 <edyta.szarowska@interia.pl>
2020-11-03 22:51:06 +0100

    Pages structure and index.js inside component folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './App.css';
-import News from './articles/containers/News';
+import News from './pages/Articles.js';
 
 
 const data = [
---
+++ src/pages/Articles.js
@@ -0,0 +1,30 @@
+/* eslint-disable linebreak-style */
+import React, { useState } from 'react';
+
+import Author from '../components/Author/Author';
+import Scoring from '../components/Scoring/Scoring';
+
+const yellow = '#f1c40f';
+
+const styles = {
+  backgroundColor: 'grey',
+  border: 'red 1px solid',
+  padding: '10px',
+  para: {
+    background: yellow
+  }
+};
+
+function News({ header, intro, author }) {
+  const [score] = useState(7);
+  return (
+    <div style={styles}>
+      <h2>{header}</h2>
+      <p style={styles.para}>{intro}</p>
+      <Author author={author} />
+      <Scoring score={score} />
+    </div>
+  );
+}
+
+export default News;
