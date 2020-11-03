874e62a2c6ff41af38ce526133039da0cb663515
Edyta2801 <edyta.szarowska@interia.pl>
2020-11-03 09:45:55 +0100

    change data

--- src/App.js
+++ src/App.js
@@ -47,12 +47,12 @@ const styles = {
 
 
 function News({ header, intro }) {
-  const [score, setScore]=useState(7);
+  const [score]=useState(7);
   return (
     <div style={styles}>
       <h2>{header}</h2>
       <p style={styles.para}>{intro}</p>
-      <p>{score}</p>
+      <p>Ocena:{score}</p>
     </div>
   );
 }
@@ -68,20 +68,19 @@ function News({ header, intro }) {
 
 
 const data = [
-  {
-    header: 'Nagłówek 1', intro: 'Intro 1',
+  {id:1, title:'Pilne: Co to był za dzień!', intro: 'Trego świat jeszcze nie widział',
   }, {
-    header: 'Nagłówek 2: Breaking news', intro: "Intro 2",
+    id: 2, title:'Wszyscy zazdroszczą Polakom!', intro: 'Takiego clickbajtowego tytułu jeszcze nikt nie widział',
   }, {
-    header: 'Nagłówek 3: Nie śpię bo trzymam kresdens', intro: "Intro 3",
+    id:3, title:'Adam Małysz zgolił wąs', intro: "Po przegranym zakładzie z Piotrem Żyłą nasz mistrz olimpijski zgolił wąsy",
   }
 ];
 
 function App() {
   return (
     <div>
-      {data.map((elem, index) => (
-        <News key={`news-${index}`} header={elem.header} intro={elem.intro} />
+      {data.map((elem) => (
+        <News key={`news-${elem.id  }`} header={elem.title} intro={elem.intro} />
       ))}
       {/* <News header="Nagłówek 1" intro="Intro 1" />
       <News header="Nagłówek 2:Breaking news" intro="Intro 2" />
