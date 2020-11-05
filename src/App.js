b15b1b80ad3e33b0ddf9354c7fcd5f94aba1dcc0
Edyta2801 <edyta.szarowska@interia.pl>
2020-11-05 09:57:25 +0100

    add Content component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 
 
 import Header from './components/Header';
-import './App.css';
+import Content from './components/Content';
 import Articles from './pages/Articles';
-
-
+import './App.css';
 
 function App() {
   return (
     <div>
-      <Header/>
-      <Articles/>
+      <Header />
+      <Content>
+        <Articles />
+      </Content>
     </div>
   );
 }
