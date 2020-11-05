0d251593bf53e8a1f7231c719371b3c68ebb4275
Edyta2801 <edyta.szarowska@interia.pl>
2020-11-05 09:48:51 +0100

    tamplate using components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import React from 'react';
 
-import './App.css';
-import News from './pages/Articles.js';
+
 import Header from './components/Header';
+import './App.css';
+import Articles from './pages/Articles';
 
 
-const data = [
-  {
-    id: 1, author: 'Jan Brzęczek', title: 'Pilne: Co to był za dzień!', intro: 'Tego świat jeszcze nie widział',
-  }, {
-    id: 2, author: 'Jan Brzęczek', title: 'Wszyscy zazdroszczą Polakom!', intro: 'Takiego clickbajtowego tytułu jeszcze nikt nie widział',
-  }, {
-    id: 3, author: 'Jan Brzęczek', title: 'Adam Małysz zgolił wąs', intro: 'Po przegranym zakładzie z Piotrem Żyłą nasz mistrz olimpijski zgolił wąsy',
-  }
-];
 
 function App() {
   return (
     <div>
       <Header/>
-      {data.map((elem) => (
-        <News
-          key={`news-${elem.id}`}
-          header={elem.title}
-          intro={elem.intro}
-          author={elem.author}
-        />
-      ))}
+      <Articles/>
     </div>
   );
 }
--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,30 +1,31 @@
-/* eslint-disable linebreak-style */
-import React, { useState } from 'react';
+import React from 'react';
 
-import Author from '../components/Author/Author';
-import Scoring from '../components/Scoring/Scoring';
+import News from '../components/News.js'
 
-const yellow = '#f1c40f';
-
-const styles = {
-  backgroundColor: 'grey',
-  border: 'red 1px solid',
-  padding: '10px',
-  para: {
-    background: yellow
+const data = [
+  {
+    id: 1, author: 'Jan Brzęczek', title: 'Pilne: Co to był za dzień!', intro: 'Tego świat jeszcze nie widział',
+  }, {
+    id: 2, author: 'Jan Brzęczek', title: 'Wszyscy zazdroszczą Polakom!', intro: 'Takiego clickbajtowego tytułu jeszcze nikt nie widział',
+  }, {
+    id: 3, author: 'Jan Brzęczek', title: 'Adam Małysz zgolił wąs', intro: 'Po przegranym zakładzie z Piotrem Żyłą nasz mistrz olimpijski zgolił wąsy',
   }
-};
+];
 
-function News({ header, intro, author }) {
-  const [score] = useState(7);
+
+function Articles() {
   return (
-    <div style={styles}>
-      <h2>{header}</h2>
-      <p style={styles.para}>{intro}</p>
-      <Author author={author} />
-      <Scoring score={score} />
+    <div >
+     {data.map((elem) => (
+        <News
+          key={`news-${elem.id}`}
+          header={elem.title}
+          intro={elem.intro}
+          author={elem.author}
+        />
+      ))}
     </div>
   );
 }
 
-export default News;
+export default Articles;
