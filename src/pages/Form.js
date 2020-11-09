8f66877e705628574e02e4d47a20684de5c12677
Edyta2801 <edyta.szarowska@interia.pl>
2020-11-10 00:02:15 +0100

    generic function for form events

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -18,14 +18,10 @@ import React from 'react';
 
 class Form extends React.Component {
     state = {
-        name: ''
+        name: '',
+        surname: '',
+        age: ''
     }
-
-    // constructor(props) {
-    //     super(props);
-    //     this.handleChange = this.handleChange.bind(this);
-
-    // }
 
     handleSubmit(event) {
         event.preventDefault();
@@ -34,18 +30,26 @@ class Form extends React.Component {
     handleClick() {
         console.log('React Button');
     }
-    handleChange(event){
-        console.log('input value:', event.target.value);
-        this.setState({ name: event.target.value });
-        console.log('this', this);
+    handleChange = (event) => {
+const keyName=event.target.name;
+        this.setState({ [keyName]: event.target.value });
+
 
     }
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <input type='text' onChange={this.handleChange.bind(this)} />
-                <button onClick={this.handleClick}>React Button</button>
+            <form onSubmit={this.handleSubmit.bind(this)}>
+                <div>
+                    <input type="text" name="name" placeholder="Name" onChange={this.handleChange} />
+                </div>
+                <div>
+                    <input type="text" name="surname" placeholder="Surname" onChange={this.handleChange} />
+                </div>
+                <div>
+                    <input type="text" name="age" placeholder="Age" onChange={this.handleChange} />
+                </div>
+                <button type="submit" onClick={this.handleClick}>Send</button>
             </form>
         );
     }
