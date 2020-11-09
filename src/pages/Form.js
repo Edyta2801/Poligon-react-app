e89bb6941d5cdda5d034edd35f6896845c78d5ee
Edyta2801 <edyta.szarowska@interia.pl>
2020-11-09 22:57:13 +0100

    add class Component Form

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,18 +1,40 @@
 import React from 'react';
 
 
-function Form() {
+// function Form() {
 
-    const handleClick = () => console.log('React button');
-    const handleSubmit = (event) => {
+//     const handleClick = () => console.log('React button');
+//     const handleSubmit = (event) => {
+//         event.preventDefault();
+//         console.log(event);
+//     };
+
+//     return (
+//         <form onSubmit={handleSubmit}>
+//             <button onClick={handleClick}>React Button</button>
+//         </form>
+//     );
+// }
+
+class Form extends React.Component {
+
+    handleSubmit(event) {
         event.preventDefault();
-        console.log(event);
-    };
+    }
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <button onClick={handleClick}>React Button</button>
-        </form>
-    );
+    handleClick() {
+        console.log('React Button');
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <button onClick={this.handleClick}>React Button</button>
+            </form>
+        );
+    }
+
 }
+
+
 export default Form;
