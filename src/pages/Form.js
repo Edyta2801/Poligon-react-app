aed662123c092152eb7c0c7595e17a07dee7a581
Edyta2801 <edyta.szarowska@interia.pl>
2020-11-10 12:58:58 +0100

    ref in class component

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -13,88 +13,100 @@ function useInput(initialValue = '') {
 
 
 
-function Form() {
+// function Form() {
 
-    const [name, handleNameChange] = useInput('');
-    const [surname, handleSurnameChange] = useInput('');
-    const [age, handleAgeChange] = useInput('');
+//     const [name, handleNameChange] = useInput('');
+//     const [surname, handleSurnameChange] = useInput('');
+//     const [age, handleAgeChange] = useInput('');
 
 
-    const handleClick = () => console.log('React button');
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(event);
-    };
-
-    // const handleChange = (event) => {
-    //     const name = event.target.name;
-    //     const value = event.target.value;
-
-    //     if (name === 'name') {
-    //         setName(value);
-    //     } else if (name === 'surname') {
-    //         setSurname(value);
-    //     } else if (name === 'age') {
-    //         setAge(value);
-
-    //     }
-    // };
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <input type="text" name="name" placeholder="Name" onChange={handleNameChange} />
-            </div>
-            <div>
-                <input type="text" name="surname" placeholder="Surname" onChange={handleSurnameChange} />
-            </div>
-            <div>
-                <input type="text" name="age" placeholder="Age" onChange={handleAgeChange} />
-            </div>
-            <button type="submit" onClick={handleClick}>Send</button>
-        </form>
-    );
-}
-
-// class Form extends React.Component {
-//     state = {
-//         name: '',
-//         surname: '',
-//         age: ''
-//     }
-
-//     handleSubmit(event) {
+//     const handleClick = () => console.log('React button');
+//     const handleSubmit = (event) => {
 //         event.preventDefault();
+//         console.log(event);
+//     };
+
+// const handleChange = (event) => {
+//     const name = event.target.name;
+//     const value = event.target.value;
+
+//     if (name === 'name') {
+//         setName(value);
+//     } else if (name === 'surname') {
+//         setSurname(value);
+//     } else if (name === 'age') {
+//         setAge(value);
+
 //     }
+// };
 
-//     handleClick() {
-//         console.log('React Button');
-//     }
-//     handleChange = (event) => {
-//         const keyName = event.target.name;
-//         this.setState({ [keyName]: event.target.value });
-
-
-//     }
-
-//     render() {
-//         return (
-//             <form onSubmit={this.handleSubmit.bind(this)}>
-//                 <div>
-//                     <input type="text" name="name" placeholder="Name" onChange={this.handleChange} />
-//                 </div>
-//                 <div>
-//                     <input type="text" name="surname" placeholder="Surname" onChange={this.handleChange} />
-//                 </div>
-//                 <div>
-//                     <input type="text" name="age" placeholder="Age" onChange={this.handleChange} />
-//                 </div>
-//                 <button type="submit" onClick={this.handleClick}>Send</button>
-//             </form>
-//         );
-//     }
-
+//     return (
+//         <form onSubmit={handleSubmit}>
+//             <div>
+//                 <input type="text" name="name" placeholder="Name" onChange={handleNameChange} />
+//             </div>
+//             <div>
+//                 <input type="text" name="surname" placeholder="Surname" onChange={handleSurnameChange} />
+//             </div>
+//             <div>
+//                 <input type="text" name="age" placeholder="Age" onChange={handleAgeChange} />
+//             </div>
+//             <button type="submit" onClick={handleClick}>Send</button>
+//         </form>
+//     );
 // }
+
+class Form extends React.Component {
+    state = {
+        name: '',
+        surname: '',
+        age: ''
+    }
+
+    constructor(props) {
+        super(props);
+        this.surnameInput = React.createRef();
+    }
+
+
+
+    handleSubmit(event) {
+        event.preventDefault();
+    }
+
+    handleClick() {
+        console.log('React Button');
+    }
+    handleChange = (event) => {
+        const keyName = event.target.name;
+        this.setState({ [keyName]: event.target.value });
+    }
+
+    handleFocusClick = () => {
+        this.surnameInput.current.focus();
+        this.surnameInput.current.value = 'Kowalski';
+        this.surnameInput.current.style.border = '#f00 1px solid';
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit.bind(this)}>
+                <div>
+                    <input type="text" name="name" placeholder="Name" onChange={this.handleChange} />
+                </div>
+                <div>
+                    <input type="text"  ref={this.surnameInput} name="surname" placeholder="Surname" onChange={this.handleChange} />
+                </div>
+                <div>
+                    <input type="text" name="age" placeholder="Age" onChange={this.handleChange} />
+                </div>
+                <button type="submit" onClick={this.handleClick}>Send</button>
+                <button type="button" onClick={this.handleFocusClick}>Focus</button>
+            </form>
+        );
+    }
+
+}
 
 
 export default Form;
