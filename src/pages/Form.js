3da12b09df6d823992a0b3f1e629697948c4fcab
Edyta2801 <edyta.szarowska@interia.pl>
2020-11-10 11:48:14 +0100

    class component Form using hooks

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,60 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
-// function Form() {
+function Form() {
 
-//     const handleClick = () => console.log('React button');
-//     const handleSubmit = (event) => {
-//         event.preventDefault();
-//         console.log(event);
-//     };
+    const [name, setName] = useState('');
+    const [surname, setSurname] = useState('');
+    const [age, setAge] = useState('');
 
-//     return (
-//         <form onSubmit={handleSubmit}>
-//             <button onClick={handleClick}>React Button</button>
-//         </form>
-//     );
-// }
 
-class Form extends React.Component {
-    state = {
-        name: '',
-        surname: '',
-        age: ''
-    }
-
-    handleSubmit(event) {
+    const handleClick = () => console.log('React button');
+    const handleSubmit = (event) => {
         event.preventDefault();
-    }
+        console.log(event);
+    };
 
-    handleClick() {
-        console.log('React Button');
-    }
-    handleChange = (event) => {
-const keyName=event.target.name;
-        this.setState({ [keyName]: event.target.value });
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
 
+        if (name === 'name') {
+            setName(value);
+        } else if (name ==='surname') {
+            setSurname(value);
+        } else if (name === 'age') {
+            setAge(value);
 
-    }
+        }
+    };
 
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit.bind(this)}>
-                <div>
-                    <input type="text" name="name" placeholder="Name" onChange={this.handleChange} />
-                </div>
-                <div>
-                    <input type="text" name="surname" placeholder="Surname" onChange={this.handleChange} />
-                </div>
-                <div>
-                    <input type="text" name="age" placeholder="Age" onChange={this.handleChange} />
-                </div>
-                <button type="submit" onClick={this.handleClick}>Send</button>
-            </form>
-        );
-    }
-
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <input type="text" name="name" placeholder="Name" onChange={handleChange} />
+            </div>
+            <div>
+                <input type="text" name="surname" placeholder="Surname" onChange={handleChange} />
+            </div>
+            <div>
+                <input type="text" name="age" placeholder="Age" onChange={handleChange} />
+            </div>
+            <button type="submit" onClick={handleClick}>Send</button>
+        </form>
+    );
 }
+
+// class Form extends React.Component {
+//     state = {
+//         name: '',
+//         surname: '',
+//         age: ''
+//     }
+
+//     handleSubmit(event) {
+//         event.preventDefault();
+//     }
+
+//     handleClick() {
+//         console.log('React Button');
+//     }
+//     handleChange = (event) => {
+//         const keyName = event.target.name;
+//         this.setState({ [keyName]: event.target.value });
+
+
+//     }
+
+//     render() {
+//         return (
+//             <form onSubmit={this.handleSubmit.bind(this)}>
+//                 <div>
+//                     <input type="text" name="name" placeholder="Name" onChange={this.handleChange} />
+//                 </div>
+//                 <div>
+//                     <input type="text" name="surname" placeholder="Surname" onChange={this.handleChange} />
+//                 </div>
+//                 <div>
+//                     <input type="text" name="age" placeholder="Age" onChange={this.handleChange} />
+//                 </div>
+//                 <button type="submit" onClick={this.handleClick}>Send</button>
+//             </form>
+//         );
+//     }
+
+// }
 
 
 export default Form;
