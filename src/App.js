9ab21ade57c39d2d3dbfdb8202ed39f3b7475c9b
Edyta2801 <edyta.szarowska@interia.pl>
2020-12-02 22:52:47 +0100

    add Browser Router

--- src/App.js
+++ src/App.js
@@ -1,31 +1,49 @@
 import React from 'react';
 import { MenuLink } from './components/MenuNav';
-import {Menu} from './components/MenuNav';
+import { Menu } from './components/MenuNav';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+
 
 import Form from './pages/Form';
 
 import Header from './components/Header';
 import Content from './components/Content';
-// import Articles from './pages/Articles';
+import Home from './pages/Home';
+import Articles from './pages/Articles';
 import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
-      <Menu>
+      {/* <Menu>
         <MenuLink to="/">Home</MenuLink>
         <MenuLink to="/about">About</MenuLink>
         <MenuLink to="/contact" isActive>Contact</MenuLink>
         <MenuLink to="/posts">Posts</MenuLink>
-      </Menu>
+      </Menu> */}
       <Content>
-        {/* <Articles /> */}
-        <Form/>
+        <Switch>
+          <Route path='/articles'>
+            <Articles />
+          </Route>
+          <Route path='/form'>
+            <Form />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
       </Content>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
